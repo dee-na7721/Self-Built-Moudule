a08b6bba37e0cfa8f1761a9d521985687f495e80
dee-na7721 <deenasitikhu@gmail.com>
2020-11-01 11:53:05 +0545

    Conversion Module added by Deena

---
+++ app.js
@@ -0,0 +1,3 @@
+let conversion = require("./log.js");
+
+console.log("Bandwidth in MB is: "+ conversion.convert(1024));
---
+++ log.js
@@ -0,0 +1,8 @@
+let data ={
+    convert : (kb)=>{
+        var mb = kb/1024;
+        return mb;
+    }
+}
+
+module.exports= data;
